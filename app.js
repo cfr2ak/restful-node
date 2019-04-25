77914e869a9fe7cf8db6f0b20795f60ca56bc151
cfr2ak <cfr2ak@gmail.com>
2019-04-25 18:29:49 +0900

    import module into app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const http = require('http');
+const httpModule = require('./modules/http-module');
 
-http.createServer(handleRequest).listen(8080, 'localhost', () => {
+const port = 8080;
+
+http.createServer(httpModule.handleRequest).listen(port, 'localhost', () => {
     console.log('Started Node.js http server at http://localhost:8080')
 });
 
