41c95a8b269e77bcc39942fc0aee29265ef6d4de
cfr2ak <cfr2ak@gmail.com>
2019-04-25 18:12:36 +0900

    restful functions

--- app.js
+++ app.js
@@ -1,11 +1,53 @@
-const http = require('http')
+const http = require('http');
 
-http.createServer((request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    response.end('Hello world!');
-    console.log('Hello handler requested');
-}).listen(8080, 'localhost', () => {
+http.createServer(handleRequest).listen(8080, 'localhost', () => {
     console.log('Started Node.js http server at http://localhost:8080')
 });
+
+function handleGetRequest(response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('GET action was requested');
+}
+
+function handlePostRequest(response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('POST action was requested');
+}
+
+function handlePutRequest(response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('PUT action was requested');
+}
+
+function handleDeleteRequest(response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('DELETE action was requested');
+}
+
+function handleBadRequest(response) {
+    console.log('Unsupported http method');
+    response.writeHead(400, {'Content-Type': 'text/plain'});
+    response.end('Bad request');
+}
+
+function handleRequest(request, response) {
+    switch (request.method) {
+        case 'GET':
+            handleGetRequest(response);
+            break;
+        case 'POST':
+            handlePostRequest(response);
+            break;
+        case 'PUT':
+            handlePutRequest(response);
+            break;
+        case 'DELETE':
+            handleDeleteRequest(response);
+            break;
+        default:
+            handleBadRequest(response);
+            break;
+    }
+    console.log('Request processing complete');
+}
+
